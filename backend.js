bf444346ef6ddedd183161d71f9c2b05c9a1d6db
Jashan Pal Singh <jsing785@mtroyal.ca>
2025-03-18 15:54:07 -0600

    Added explanatory comments and references.

--- backend.js
+++ backend.js
@@ -1,13 +1,20 @@
-let adBlockEnabled = true;
-let blockedDomains = [];
-let blockedTrackers = [];
+/* This is the brains behind most of the functionality in our Chrome extension. We mentioned this file in our 
+manifest.JSON to tell the browser that this file will govern all the services provided by our extension.*/
 
-// Fetch all domains from the blocked_domains.json.
+//First, we define some variables that we will use within this file.
+let adBlockEnabled = true;  // Variable that decides, the current status of out ad-block switch, we initialize it as true be default and update it as user changes preferences.
+let blockedDomains = [];    // This is an empty list that will soon contain about six thousand entries of blocked domains as we fetch them from our blocked_domains.JSON.
+let blockedTrackers = [];   // This list will contain over fifteen thousand nefarious trackers present in our blocked-trackers.JSON.
+
+/* Fetch all domains from the blocked_domains.json.
+   the getURL method returns a String of content present in the mentioned URL and thus, needs to be converted to JSON again.
+   Source: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/getURL 
+   We then simply set the value of our variable defined earlier to the attribute present in our JSOn, which is also a list. */
 fetch (chrome.runtime.getURL("blocked_domains.json")).then(response => response.json())
 .then(data => { blockedDomains = data.domains; })
 .catch(error => console.error("Could not load domains", error));
 
-// Fetch all trackers from the blocked_trackers.json.
+// Fetch all trackers from the blocked_trackers.json. Same woring as the other list variable.
 fetch (chrome.runtime.getURL("blocked_trackers.json")).then(response => response.json())
 .then(data => { blockedTrackers = data.trackers; })
 .catch(error => console.error("Could not load trackers", error));
@@ -22,7 +29,10 @@ chrome.storage.sync.get("adBlockToggle", (result) => {
     }
 });
 
-//Listen for change of state from home.js
+/*Listen for change of state from home.js 
+As mentioned in the home.js, chrome.runtime.onMessage listens for any messages broadcasted by other functions.
+If the message contains our desired content, we once again set the updated parameter in browser storage and call in functions to
+implement the ad blocking, tracker blocking, and third-party cookie blocking functionality */
 chrome.runtime.onMessage.addListener((message) =>{
     if(message.action === "toggleAdBlock"){
         adBlockEnabled = message.enabled;
--- home.js
+++ home.js
@@ -1,11 +1,16 @@
+/* This file communicates instructions between the index.html front-end and the backend.js back-end.
+First, we add an event listner to wait for all DOM content to load before enabling any scripts to prevent from
+generating unnecesary errors as variables will not be able to find associated objects in html file.*/
 document.addEventListener("DOMContentLoaded", () => {
 
     //------------------------------------AD BLOCKER---------------------------------------------------------//
-    const adBlockCheckbox = document.querySelector("#toggleAdBlock");
+
+    const adBlockCheckbox = document.querySelector("#toggleAdBlock"); // Select the checkbox we created for our extension's main events.
 
 
-    //Find the value of the ad blocker toggle switch present in chome storage. Create an anonymous function that
-    //checks the last state of it. If it was toggled on, we set the current state to on, same for off. For undefined (during the first setup), we let it be on by default.
+    /*Find the value of the ad blocker toggle switch present in chome storage using the get method. Create an anonymous function that
+    checks the last state of it. If it was toggled on, we set the current state to on, same for off. For undefined (usually during the first setup),
+    we let it be on by default. Source: https://developer.chrome.com/docs/extensions/reference/api/storage#property-sync */
     chrome.storage.sync.get("adBlockToggle", (result) => {
         if(result.adBlockToggle !== undefined){
             adBlockCheckbox.checked = result.adBlockToggle;
@@ -15,7 +20,11 @@ document.addEventListener("DOMContentLoaded", () => {
             chrome.storage.sync.set({adBlockToggle: true});
         }
     });
-    // Adding an event listner to the ad blocking checkbox that detects user-made change to checkbox state and updates the state to chrome storage.
+    /* Adding an event listner to the ad blocking checkbox that detects user-made change to checkbox state and updates the state to chrome storage. 
+    It also sends out a message during the runtime to any event listners in our application. The message contains a simple JSON containing 
+    an "action" field and an "enabled" field (these are just the names we used, field names can be anything). We will place a similar runtime.onMessage 
+    listener in our back-end to wait for this message and take necessary action.
+    Source: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/sendMessage */
     adBlockCheckbox.addEventListener("change", () => {
         // console.log("It is working");
         const isEnabled = adBlockCheckbox.checked;
@@ -23,25 +32,35 @@ document.addEventListener("DOMContentLoaded", () => {
         chrome.runtime.sendMessage({action: "toggleAdBlock", enabled: isEnabled});
     });
 
-
+    //---------------------------------END OF AD BLOCKER---------------------------------------------------//
     //---------------------------------HAVE I BEEN PWNED?--------------------------------------------------//
-    const checkPwnedButton = document.querySelector("#checkpwned");
+
+    /* For our project, we also decided to give our users the convenience of searching for their mail involved in any recorded data breaches.
+    "Have I Been Pwned?" provides a convenient API that allows us to query theit database by sending it any email address. If present in the database,
+    we will receive a JSON containing all the information present about our provided email address. This functionality requires purchasing an API key.
+    Source: https://haveibeenpwned.com/api/v3 */
+    const checkPwnedButton = document.querySelector("#checkpwned"); // Select elements in the HTML and assign them variablees.
     const showPwnedButton = document.querySelector("#showPwned");
     const inputEmail = document.querySelector("#email");
     const pwnedDiv = document.querySelector("#pwnedDiv");
     const emailDiv = document.querySelector("#pwned");
 
+    // Add an event listener to our "is my email safe?" button to un-hide the div containing our API functionality. It remains hidden when not in use to maintain cleanliness.
     showPwnedButton.addEventListener("click", () => {
         emailDiv.className = "";
     });
 
+    /*Within the API div, we add an event listener to trigger an asynchronus function that sends a web request to HIBP servers. Upon receiving the response,
+    we update the content displayed in th API div with either the breaches, formatted in a list, or a simple message telling users the good news. */ 
     checkPwnedButton.addEventListener("click", async () => {
-        const email = inputEmail.value.trim();
+        const email = inputEmail.value.trim(); // assign user input to a variable
         if(!email){
-            pwnedDiv.innerHTML = "<p>Please enter a valid email address.</p>";
+            pwnedDiv.innerHTML = "<p>Please enter a valid email address.</p>";  //if user leaves the field empty, display message.
             return;
         }else{
-                pwnedDiv.textContent = "";
+                pwnedDiv.textContent = "";  //Empty the div of any previous search content before making the request.
+
+                // Fetch a response from the API servers, the format of querying is provided by HIBP documentation. Source: https://haveibeenpwned.com/api/v3 
                 const response = await fetch(`https://haveibeenpwned.com/api/v3/breachedaccount/${encodeURIComponent(email)}?truncateResponse=false`, {
                  method: "GET",
                  headers: {
@@ -49,9 +68,11 @@ document.addEventListener("DOMContentLoaded", () => {
                     "User-Agent": "Raaz-Extension/1.0"
                 }   
                 });
+                //If we do not receive anything, the email is not present in any breaches.
                 if(response.status == 404){
                     pwnedDiv.innerHTML = "Your email has not been found in any breaches.";
                 }
+                //Otherwise, we create a list of breaches from the received JSON and append the data to our API div.
                 else if(response.ok){
                     const breaches = await response.json();
                     let breachHeading = document.createElement("p");
@@ -65,7 +86,8 @@ document.addEventListener("DOMContentLoaded", () => {
                     let breachNotification = document.createElement("p");
                     breachNotification.textContent = "Please change your passwords for the above websites immediately!";
                     pwnedDiv.append(breachHeading, breachList, breachNotification);
-                }else{
+                } // Finally, we catch any errors.
+                else{
                     throw new Error(`Error ${response.status}: ${response.statusText}`);
                 }
         }
